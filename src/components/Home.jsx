37be794e9c9b4e001c5f69514ac58cb61ea37549
Spencer Lyons <sjl8r8@gmail.com>
2020-06-23 15:01:56 -0500

    Home image now darken rather than being switched out

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Nav from "./Nav";
 
-const Home = props => {
+const Home = (props) => {
   return (
-    <div style={{position:'relative', backgroundImage:props.darkMode ? `url(${require('../images/waterfallReduced-dark.jpg')})` : `url(${require('../images/waterfallReduced.jpg')})`}} className="bgImage">
-      {/* <img src={props.darkMode ? require('../images/waterfallReduced-dark.jpg') : require('../images/waterfallReduced.jpg')} className="bgImage" alt="waterfall"/> */}
-      <Nav darkMode={props.darkMode} onDarkChange={props.onDarkChange}/>
-      <div className="homeTextContainer" style={styles.textContainer}>
-        <h1 className="mainTitle">Hi! I'm <span style={{color:props.darkMode ? '#b43088' : 'white'}}>Spencer Lyons</span></h1>
-        <h3 className="subTitle" >A Software Devloper</h3>
+    <div
+      style={{
+        position: "relative",
+        backgroundImage: `url(${require("../images/waterfallReduced.jpg")})`,
+      }}
+      className="bgImage"
+    >
+      <Nav darkMode={props.darkMode} onDarkChange={props.onDarkChange} />
+        <div className="homeTextContainer" style={styles.textContainer}>
+          <h1 className="mainTitle">
+            Hi! I'm{" "}
+            <span style={{ color: props.darkMode ? "#b43088" : "white" }}>
+              Spencer Lyons
+            </span>
+          </h1>
+          <h3 className="subTitle">A Software Devloper</h3>
+        </div>
+      <div style={{...styles.blackCover, opacity:props.darkMode ? '60%' : '25%'}}>
       </div>
     </div>
   );
@@ -16,14 +28,22 @@ const Home = props => {
 
 const styles = {
   textContainer: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%'
-  }
-}
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    zIndex: 5,
+  },
+  blackCover: {
+    backgroundColor: "black",
+    width: "100%",
+    height: "100%",
+    opacity: '75%',
+    zIndex: 0,
+  },
+};
 
 export default Home;
